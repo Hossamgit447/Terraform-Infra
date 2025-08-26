32a2b12b51d9c254c7ab0fb2b0ffc31978396c63
Hossamgit447 <hossamgithub@gmail.com>
2025-08-25 17:30:01 -0700

    spelling fix

--- projects/fintech/prod/main.tf
+++ projects/fintech/prod/main.tf
@@ -3,9 +3,8 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   env      = var.env
 }
-/*module "db_password_secret" {
+module "db_password_secret" {
   source      = "git::https://github.com/Hossamgit447/terraform-modules.git//modules/secret_manager?ref=master"
   name        = var.name
   description = "Secret container for MyApp DB password"
 }
-*/
