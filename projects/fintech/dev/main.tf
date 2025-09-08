72dce50dce288ed47aa491113f5ee16c35e0221e
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 09:12:17 -0700

    asd

--- projects/fintech/dev/main.tf
+++ projects/fintech/dev/main.tf
@@ -37,7 +37,7 @@ module "db_password_secret" {
 }
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
-  depends_on = [module.eks] 
+ # depends_on = [module.eks] 
 }
 
 data "aws_eks_cluster_auth" "this" {
