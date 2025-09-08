d3e3d5a5fef00c284cdbd85cc7d4fbc072f09e25
Hossamgit447 <hossamgithub@gmail.com>
2025-09-08 08:16:37 -0700

    aaa

--- projects/fintech/dev/main.tf
+++ projects/fintech/dev/main.tf
@@ -2,11 +2,22 @@
 # Network Module
 # ---------------------
 module "network" {
-  source = "git::https://github.com/Hossamgit447/terraform-modules.git//modules/network?ref=master"
-  env= var.env
-  aws_region = var.aws_region
-  vpc_cidr   = var.vpc_cidr
-  azs   = var.azs
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  private_subnets = ["10.0.101.0/24","10.0.102.0/24"]
+  public_subnets  = ["10.0.1.0/24",  "10.0.2.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 # ---------------------
